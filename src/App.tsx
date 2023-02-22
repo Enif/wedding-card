55d807735c40e2ae7ff72c08394332e2e18e4fc0
yjkim <yjkim1803@gmail.com>
2023-02-22 09:41:43 +0900

    tmp: working in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import comingSoonImg from './assets/coming_soon.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={comingSoonImg} className="coming-soon-img" alt="logo" />
       </header>
     </div>
   );
